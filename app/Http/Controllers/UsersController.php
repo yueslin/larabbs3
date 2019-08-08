f52836673cee1f0ca884e7f04551ba3d1a0edc94
17853 <1785301169@qq.com>
2019-08-08 17:07:48 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -19,9 +20,18 @@ class UsersController extends Controller
 
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
-        $user->update($request->all());
+
+        $data = $request->all();
+
+        if ($request->avatar){
+            $resule = $uploader->save($request->avatar,'avatars',$user->id);
+            if ($resule){
+                $data['avatar'] = $resule['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
