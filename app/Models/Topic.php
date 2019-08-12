7c44f8ee3372eb29136c7d1d9d5289178d554c2c
17853 <1785301169@qq.com>
2019-08-12 11:45:36 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,5 +9,14 @@ class Topic extends Model
         'last_reply_user_id', 'order', 'excerpt', 'slug'
     ];
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
