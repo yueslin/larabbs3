3167c50ab29241cef85a97e2950cce8dc965a963
17853 <1785301169@qq.com>
2019-08-19 18:18:07 +0800

    修复 xss 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,10 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+
+        // clean 过滤 XSS 注入
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 
