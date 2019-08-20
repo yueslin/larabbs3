5df7d00431ffc274a61abb99c602aac251a6ad8e
17853 <1785301169@qq.com>
2019-08-20 15:40:58 +0800

    用户可以编辑话题

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,10 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
-
         // clean 过滤 XSS 注入
         $topic->body = clean($topic->body, 'user_topic_body');
-
         $topic->excerpt = make_excerpt($topic->body);
     }
 
