25480254df48a2c28480050a275a7afa6917240a
17853 <1785301169@qq.com>
2019-09-25 17:17:51 +0800

    replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\ReplyRequest;
+use App\Http\Requests\Request;
 use App\Models\Reply;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 
 class RepliesController extends Controller
 {
+
+    public function index(Topic $topic,Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $topic->replies()->paginate(20);
+
+        if ($replies->include){
+            $replies->load(explode(',',$request->include));
+        }
+
+
+        return $this->response->paginator($replies,new ReplyTransformer());
+
+    }
+
+    public function userIndex(User $user,Request $request)
+    {
+
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies,new ReplyTransformer());
+    }
+
+
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
         $reply->content = $request->contents;
